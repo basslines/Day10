849806b39eaa747269cea0ef65b574805976a8ea
basslines <admir.osmanovic@gmail.com>
2021-03-17 19:46:14 +0100

    dodao index.js fajl

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function getUsername(email) {
+  var username = "";
+
+  var username = {
+    user1: "user1@mail.com",
+    user2: "user2@mail.com",
+    user3: "user3@mail.com",
+    user4: "user4@mail.com",
+    user5: "user5@mail.com",
+    user6: "user6@mail.com",
+  };
+  return username;
+}
+
+console.log(getUsername("user1@mail.com"));
+
+module.exports = getUsername;
