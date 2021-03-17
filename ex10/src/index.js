04c912a95bbbea0d2366fb3f891ef827c8d70883
basslines <admir.osmanovic@gmail.com>
2021-03-17 21:16:22 +0100

    dodao index.js fajl

---
+++ ex10/src/index.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  var myMusic = {
+    001: {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP",
+      },
+      gold: true,
+    },
+    002: {
+      artist: "Steely Dan",
+      title: "Josie",
+      release_year: 1972,
+      id: 1214,
+      formats: {
+        1: "CD",
+        2: "LP",
+        3: "USB",
+      },
+    },
+  };
+}
+myFunction()[2];
+module.exports = myFunction;
